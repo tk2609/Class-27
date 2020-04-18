674c1458cfe5346ad21819fd975bf14ffc0ecd98
tk2609 <uma.kosaraju@gmail.com>
2020-04-18 10:42:38 +0530

    display the files

--- Ground.js
+++ Ground.js
@@ -11,7 +11,7 @@ class Ground {
     display(){
       var pos =this.body.position;
       rectMode(CENTER);
-      fill("brown");
+      fill("pink");
       rect(pos.x, pos.y, this.width, this.height);
     }
   };
